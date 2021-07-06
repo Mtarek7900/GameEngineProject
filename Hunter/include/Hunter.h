2554bd16d102dfd20f084a52b5325b03bf5914bf
Mtarek7900 <mohammed.tarek03@myhunter.cuny.edu>
2021-07-06 17:35:07 -0400

    initial commit for game engine, the basic structure is created

---
+++ Hunter/include/Hunter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <HunterCentral.h>
+#include <HunterApp.h>
+
+#define START_GAME(classname) \
+	int main()\
+	{\
+		classname::Init();\
+		classname::GetApplication()->RunGame();\
+	}
---
+++ Hunter/src/HunterApp.cpp
@@ -0,0 +1,30 @@
+#include <pch.h>
+#include "HunterApp.h"
+
+namespace Hunter 
+{
+	void HunterApp::RunGame()
+	{
+		HLOG("Start the game")
+		while (true)
+		{
+
+		}
+	}
+
+	HunterApp* HunterApp::GetApplication()
+	{
+		return instance;
+	}
+
+	void HunterApp::Init()
+	{
+		if (instance != nullptr)
+			instance = new HunterApp;
+	}
+
+	HunterApp::HunterApp() 
+	{
+		assert(instance == nullptr);
+	}
+}
